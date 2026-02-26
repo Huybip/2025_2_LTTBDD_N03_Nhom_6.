86202a88be20fb483d9fc732f07d372deb5d3b5a
Huybip <155954088+Huybip@users.noreply.github.com>
2026-02-26 18:36:35 +0000

    main.dart

--- final_exam_project/lib/main.dart
+++ final_exam_project/lib/main.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MusicApp());
 
 class MusicApp extends StatelessWidget {
-  const MusicApp({Key? key}) : super(key: key);
+  const MusicApp({super.key});         
 
   @override
   Widget build(BuildContext context) {
-	return MaterialApp(
-	  title: 'Music App',
-	  home: Scaffold(
-		appBar: AppBar(title: const Text('Music App')),
-		body: const Center(child: Text('Welcome')),
-	  ),
-	);
+    return MaterialApp(
+      title: 'Music App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Music App')),
+        body: const Center(child: Text('Welcome')),
+      ),
+    );
   }
 }
