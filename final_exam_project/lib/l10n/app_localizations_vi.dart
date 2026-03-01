891f6c3826af6d6effed5021d1f34281b20201a7
lhgiangg <glethihuong538@gmail.com>
2026-03-01 23:04:56 +0700

    Add app_localizations.dart

---
+++ final_exam_project/lib/l10n/app_localizations_vi.dart
@@ -0,0 +1,67 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (vi).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get appTitle => 'Ứng dụng Nhạc';
+
+  @override
+  String get tabHome => 'Trang chủ';
+
+  @override
+  String get tabDiscovery => 'Khám phá';
+
+  @override
+  String get tabAccount => 'Tài khoản';
+
+  @override
+  String get tabSettings => 'Cài đặt';
+
+  @override
+  String get loadingSongs => 'Đang tải danh sách bài hát…';
+
+  @override
+  String get settingsTitle => 'Cài đặt';
+
+  @override
+  String get language => 'Ngôn ngữ';
+
+  @override
+  String get languageEnglish => 'Tiếng Anh';
+
+  @override
+  String get languageVietnamese => 'Tiếng Việt';
+
+  @override
+  String get groupInfo => 'Thông tin nhóm';
+
+  @override
+  String get groupInfoTitle => 'Giới thiệu nhóm';
+
+  @override
+  String get discoveryTitle => 'Khám phá';
+
+  @override
+  String get accountTitle => 'Tài khoản';
+
+  @override
+  String get notImplementedYet => 'Chưa triển khai';
+
+  @override
+  String get appearance => 'Giao diện';
+
+  @override
+  String get themeSystem => 'Theo giao diện máy';
+
+  @override
+  String get themeLight => 'Giao diện sáng';
+
+  @override
+  String get themeDark => 'Giao diện tối';
+}
